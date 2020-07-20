c00b34d2da2c49b736d5e03e1f967c3384280afd
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-07-20 19:13:42 +0900

    added example that add s3 event to lambda function.

--- cdk_lambda/cdk_lambda_stack.py
+++ cdk_lambda/cdk_lambda_stack.py
@@ -1,5 +1,9 @@
-from aws_cdk import core
-from aws_cdk import aws_lambda as _lambda
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+    aws_s3 as _s3,
+    aws_s3_notifications
+)
 
 
 class CdkLambdaStack(core.Stack):
@@ -17,3 +21,9 @@ class CdkLambdaStack(core.Stack):
 
         # 環境変数を追加
         lambdaFn.add_environment(key="STAGE", value="DEV")
+
+        # s3バケットを作成し、通知イベントを設定
+        bucket = _s3.Bucket(self, "SampleBucket", bucket_name="kimi-first-cdk-bucket")
+        notification = aws_s3_notifications.LambdaDestination(lambdaFn)
+        bucket.add_event_notification(_s3.EventType.OBJECT_CREATED, notification, _s3.NotificationKeyFilter(prefix="hoge", suffix=".csv"))
+
