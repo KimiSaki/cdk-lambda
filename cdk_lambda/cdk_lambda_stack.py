d2cfcf7b9aa16686206b620a272f975e7d967801
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-07-20 18:02:26 +0900

    first commit.

--- cdk_lambda/cdk_lambda_stack.py
+++ cdk_lambda/cdk_lambda_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_lambda as _lambda
 
 
 class CdkLambdaStack(core.Stack):
@@ -7,3 +8,9 @@ class CdkLambdaStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        lambdaFn = _lambda.Function(self, "SampleLambdaFunction", 
+            code=_lambda.Code.from_asset('function/'),
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            handler="index.lambda_handler",
+            function_name="sample_lambda_function"
+        )
