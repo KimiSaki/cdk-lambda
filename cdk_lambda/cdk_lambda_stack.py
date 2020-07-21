1e8626a02a5ad161980c8ab76f9f1b341810b335
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-07-21 11:21:32 +0900

    added cloudwatch events.

--- cdk_lambda/cdk_lambda_stack.py
+++ cdk_lambda/cdk_lambda_stack.py
@@ -2,7 +2,9 @@ from aws_cdk import (
     core,
     aws_lambda as _lambda,
     aws_s3 as _s3,
-    aws_s3_notifications
+    aws_s3_notifications,
+    aws_events as _events,
+    aws_events_targets as _targets
 )
 
 
@@ -26,4 +28,11 @@ class CdkLambdaStack(core.Stack):
         bucket = _s3.Bucket(self, "SampleBucket", bucket_name="kimi-first-cdk-bucket")
         notification = aws_s3_notifications.LambdaDestination(lambdaFn)
         bucket.add_event_notification(_s3.EventType.OBJECT_CREATED, notification, _s3.NotificationKeyFilter(prefix="hoge", suffix=".csv"))
+
+        # 定期的に実行するイベントを設定
+        rule = _events.Rule(self, "SampleEventRule", 
+            rule_name="schedule_trigger_event",
+            schedule=_events.Schedule.expression("cron(10 * * * ? *)")
+        )
+        rule.add_target(_targets.LambdaFunction(lambdaFn))
 
